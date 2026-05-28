03d73cb14ff6d0dcf705ecc3c21330eab2b3ea6e
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-28 23:31:04 +0700

    Add client session model

--- client/session.py
+++ client/session.py
@@ -1,1 +1,12 @@
+import time
+
+
+class ClientSession:
+    def __init__(self):
+        self.connected_at = time.time()
+        self.last_seen = time.time()
+
+    def update_activity(self):
+        self.last_seen = time.time()
+
 
