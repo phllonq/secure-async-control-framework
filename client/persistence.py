353f39f96d3379422cbf9d01826b3f09a3eeb0df
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-28 23:31:24 +0700

    Add client persistence layer

--- client/persistence.py
+++ client/persistence.py
@@ -1,1 +1,30 @@
+import json
+from pathlib import Path
+
+
+class PersistenceLayer:
+    STATE_FILE = "runtime/client_state.json"
+
+    @staticmethod
+    def save(state: dict):
+        Path("runtime").mkdir(exist_ok=True)
+
+        with open(
+            PersistenceLayer.STATE_FILE,
+            "w"
+        ) as file:
+            json.dump(state, file)
+
+    @staticmethod
+    def load():
+        path = Path(
+            PersistenceLayer.STATE_FILE
+        )
+
+        if not path.exists():
+            return {}
+
+        with open(path, "r") as file:
+            return json.load(file)
+
 
