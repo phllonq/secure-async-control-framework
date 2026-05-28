9bc81d609ff0f7e72dd191e532cad6772e096566
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-28 23:30:45 +0700

    Add heartbeat management component

--- client/heartbeat.py
+++ client/heartbeat.py
@@ -1,1 +1,18 @@
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class HeartbeatManager:
+    INTERVAL = 30
+
+    async def start(self):
+        while True:
+            logger.info("Heartbeat transmitted")
+
+            await asyncio.sleep(
+                self.INTERVAL
+            )
+
 
