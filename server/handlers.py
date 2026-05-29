3f8d7d1f566d44507669fcee7190e4c552b6a692
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-29 13:27:23 +0700

    Add packet handling interface

--- server/handlers.py
+++ server/handlers.py
@@ -1,1 +1,15 @@
+class PacketHandler:
+    async def handle(
+        self,
+        packet: dict
+    ):
+        packet_type = packet.get(
+            "packet_type"
+        )
+
+        return {
+            "handled": True,
+            "type": packet_type
+        }
+
 
