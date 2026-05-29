66c79a6fdb3f32abd04471e51a481c08eec84776
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-29 13:26:44 +0700

    Add retry queue implementation

--- server/retry_queue.py
+++ server/retry_queue.py
@@ -1,1 +1,20 @@
+from collections import deque
+
+
+class RetryQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def push(
+        self,
+        item
+    ):
+        self.queue.append(item)
+
+    def pop(self):
+        if not self.queue:
+            return None
+
+        return self.queue.popleft()
+
 
