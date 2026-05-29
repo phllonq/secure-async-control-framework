dfded1bf06ae3c59745c375caca57ac406ca5f05
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-29 13:35:00 +0700

    Add application entry point

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import asyncio
+
+from server.server import AsyncC2Server
+
+
+async def main():
+    server = AsyncC2Server(
+        certfile="certs/server.pem",
+        keyfile="certs/server.key"
+    )
+
+    await server.start(
+        host="0.0.0.0",
+        port=4444
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
 
