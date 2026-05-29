96fb79010bf5177c3ee180e42356d5a30a37e8a0
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-29 13:25:07 +0700

    Add session management model

--- server/session.py
+++ server/session.py
@@ -1,1 +1,24 @@
+import time
+
+
+class ClientSession:
+    def __init__(
+        self,
+        client_id: str
+    ):
+        self.client_id = client_id
+
+        self.connected_at = time.time()
+        self.last_seen = time.time()
+
+        self.sequence = 0
+
+    def touch(self):
+        self.last_seen = time.time()
+
+    def next_sequence(self):
+        self.sequence += 1
+
+        return self.sequence
+
 
