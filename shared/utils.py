0f7b944670fa76052531f8040b245c9b8fb3bb60
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-29 13:30:10 +0700

    Add shared utility helpers

--- shared/utils.py
+++ shared/utils.py
@@ -1,1 +1,15 @@
+import logging
+
+
+def configure_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=(
+            "%(asctime)s - "
+            "%(levelname)s - "
+            "%(name)s - "
+            "%(message)s"
+        )
+    )
+
 
