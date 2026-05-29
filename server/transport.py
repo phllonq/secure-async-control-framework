5714aff1ba23418db4180c5144354ef3cbff32c6
Dino <190559996+phllonq@users.noreply.github.com>
2026-05-29 13:25:32 +0700

    Add packet transport abstraction

--- server/transport.py
+++ server/transport.py
@@ -1,1 +1,34 @@
+import struct
+
+
+class PacketTransport:
+    HEADER_SIZE = 4
+
+    @staticmethod
+    def encode(
+        payload: bytes
+    ) -> bytes:
+        return (
+            struct.pack(
+                "!I",
+                len(payload)
+            ) + payload
+        )
+
+    @staticmethod
+    async def receive(
+        reader
+    ) -> bytes:
+        header = await reader.readexactly(
+            PacketTransport.HEADER_SIZE
+        )
+
+        length = struct.unpack(
+            "!I",
+            header
+        )[0]
+
+        return await reader.readexactly(
+            length
+        )
 
